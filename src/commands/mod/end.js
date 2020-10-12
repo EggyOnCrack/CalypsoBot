f5236e6d5a5d6546d448bd514b19bd48758a5d02
EggyOnCrack <69737561+EggyOnCrack@users.noreply.github.com>
2020-10-12 19:13:41 +1300

    h

--- src/commands/mod/end.js
+++ src/commands/mod/end.js
@@ -1,34 +1,33 @@
 const Command = require('../Command.js');
 const { MessageEmbed } = require('discord.js');
 
-module.exports = class startCommand extends Command {
+module.exports = class endCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'start',
+      name: 'end',
       aliases: [],
-      usage: 'start',
-      description: 'Starts a session for Greenville Role-Play',
+      usage: 'end',
+      description: 'Ends a session for Greenville Role-Play',
       type: client.types.MOD,
       clientPermissions: ['SEND_MESSAGES', 'EMBED_LINKS', 'MANAGE_ROLES'],
       userPermissions: ['MANAGE_ROLES'],
-      examples: ['start']
+      examples: ['end']
     });
   }
   async run(message, args) {
 
         const embed = new MessageEmbed()
-          .setTitle('Sessions Started!')
-          .setDescription(`${message.author} has started a session!`)
+          .setTitle(':x: Sessions Ended! :x:')
+          .setDescription(`${message.author} has unfortunately ended the current session. `)
           .addField('Moderator', message.member, true)
-          .addField('Server', 'Code/Link will be released by the hoster once there are 7 reactions')
-          .addField('✅React now!✅', 'Must have 7 reactions to continue the action!')
+          .addField('Next Server?', 'The Next server will be posted in about 4 hours.')
           .setImage('https://t6.rbxcdn.com/b6ed73502cb54bbb0c141a391025933f')
           .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
           .setTimestamp()
           .setColor(message.guild.me.displayHexColor);
           message.react('✅');
-          message.channel.send(embed).then(embedMessage => {
-            embedMessage.react("❌")
-          })
-        }
-    }
+        message.channel.send(embed).then(embedMessage => {
+            embedMessage.react("❌");
+        });
+      }
+    }  
