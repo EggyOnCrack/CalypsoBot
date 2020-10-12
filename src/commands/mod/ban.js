e6abcc01cbcd90c3b05c495e1aa2e15535b0864a
EggyOnCrack <69737561+EggyOnCrack@users.noreply.github.com>
2020-10-12 17:55:35 +1300

    h

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -14,6 +14,8 @@ module.exports = class BanCommand extends Command {
     });
   }
   async run(message, args) {
+
+    const member = this.getMemberFromMention(message, args[0]) || message.guild.members.cache.get(args[0]);
     if (!member)
       return this.sendErrorMessage(message, 0, 'Please mention a user or provide a valid user ID');
     if (member === message.member)
@@ -26,11 +28,6 @@ module.exports = class BanCommand extends Command {
     let reason = args.slice(1).join(' ');
     if (!reason) reason = '`None`';
     if (reason.length > 1024) reason = reason.slice(0, 1021) + '...';
-    const reactions = await message.awaitReactions(reaction => {
-      console.log(reaction)
-      return reaction.name === ":white_check_mark:"
-    } , {time: 7000});
-    const member = this.getMemberFromMention(message, args[0]) || message.guild.members.cache.get(args[0]);
     
     await member.ban({ reason: reason });
 
